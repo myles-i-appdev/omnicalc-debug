290ccb6391b94c280ba2f9e32da3636040b85b81
Myles Iribarne <myles.iribarne@gmail.com>
2020-07-30 18:56:01 -0700

    updated

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,21 +1,21 @@
 class CalendarController < ApplicationController
-  
+
   def invite
-    
+
     render({ :template => "calendar_templates/new.html.erb"})
   end
 
   def generate
-    @name = params.fetch("event_name")
-    @details = params.fetch("detail")
-    @location = params.fetch("event_location")
+    @name = params.fetch("name")
+    @details = params.fetch("event_details")
+    @location = params.fetch("location")
 
-    regular_start_date = params.fetch("start_time")
+    regular_start_date = params.fetch("event_begins")
     @start_date = regular_start_date.to_s.gsub("-", "").gsub(":", "") + "00Z"
 
-    regular_end_date = params.fetch("end_time")
+    regular_end_date = params.fetch("event_ends")
     @end_date = regular_end_date.to_s.gsub("-", "").gsub(":", "") + "00Z"
-    
+
     @invite = "https://www.google.com/calendar/render?action=TEMPLATE&text=#{@name}&details=#{@details}&location=#{@location}&dates=#{@start_date}/#{@end_date}"
 
     render({ :template => "calendar_templates/results.html.erb"})
